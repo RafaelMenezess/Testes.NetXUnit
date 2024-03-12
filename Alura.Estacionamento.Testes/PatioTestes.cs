1b949bd157c605ee753323b581870054ef6c6c67
Rafael Menezes <rafa.rcm@hotmail.com>
2024-03-12 07:11:53 -0300

    Utilizando xUnit

---
+++ Alura.Estacionamento.Testes/PatioTestes.cs
@@ -0,0 +1,32 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            Patio estacionamento = new Patio();
+            Veiculo veiculo = new Veiculo();
+            veiculo.Proprietario = "Rafael";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Prata";
+            veiculo.Modelo = "Celta";
+            veiculo.Placa = "AAA-9999";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+
+        }
+    }
+}
