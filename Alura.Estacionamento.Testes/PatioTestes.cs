90f159ba6db87a9ae33274af449ff1678a722a68
Rafael Menezes <rafa.rcm@hotmail.com>
2024-03-12 07:21:35 -0300

    Anotação InlineData

--- Alura.Estacionamento.Testes/PatioTestes.cs
+++ Alura.Estacionamento.Testes/PatioTestes.cs
@@ -28,5 +28,30 @@ namespace Alura.Estacionamento.Testes
             Assert.Equal(2, faturamento);
 
         }
+
+        [Theory]
+        [InlineData("José", "BBB-1111", "preto", "Gol")]
+        [InlineData("Maria", "CCC-2222", "branco", "Palio")]
+        [InlineData("Carlos", "DDD-3333", "cinza", "Vectra")]
+        public void ValidaFaturamentoComVariosVeiculos(string proprietario,
+            string placa, string cor, string modelo)
+        {
+            //Arrange
+            Patio estacionamento = new Patio();
+            Veiculo veiculo = new Veiculo();
+            veiculo.Proprietario = proprietario;
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+            veiculo.Placa = placa;
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+        }
     }
 }
